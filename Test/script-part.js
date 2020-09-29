cca3f0edc8bc3fa73cb306cc432d6ca4fcde24f0
VesselForScience <sciencevessel@protonmail.com>
2020-09-29 20:47:58 -0300

    JS part almost complete.

--- Test/script-part.js
+++
@@ -1,8 +0,0 @@
-
-function CA(){
-    var testfn = document.getElementById("inpFN")
-    var textres = document.getElementById("textdiv-res") 
-    var testsn = document.getElementById("inpSN")
-
-    textres.innerText = `${Number(testfn.value)*Number(testsn.value)}`
-}
---
+++ Test/script.js
@@ -0,0 +1,17 @@
+var valueN;
+
+//Function bot(numP) gets the value of each button, based on its respective "value=""" (nump)
+function bot(numP){
+    valueN = document.Calculator.numbers.value += numP;
+
+}
+
+//Clears.
+function cle(){
+    valueN = document.Calculator.numbers.value = '';
+
+}
+//Couldn't find a substitute for eval(.)
+function ress(){
+    
+}
